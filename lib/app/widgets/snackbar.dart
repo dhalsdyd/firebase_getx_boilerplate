9348a5f391b3e88d5f8c434066236081b539a23d
Minyong Oh <dhalsdyd@gmail.com>
2022-10-05 15:12:26 +0900

    Update snackbar widget style

--- lib/app/widgets/snackbar.dart
+++ lib/app/widgets/snackbar.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_getx_boilerplate/app/core/theme/color_theme.dart';
-import 'package:firebase_getx_boilerplate/app/core/theme/text_theme.dart';
-import 'package:firebase_getx_boilerplate/app/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class FGBPSnackBar {
-  static void open(String title,
-      {Color backgroundColor = AppColorTheme.black,
-      Color textColor = AppColorTheme.black}) {
+  static void open(
+    String title, {
+    Color backgroundColor = FGBPColors.mainColor,
+    Color textColor = Colors.white,
+  }) {
     Get.rawSnackbar(
       titleText: Text(title,
           style: TextStyle(
@@ -24,48 +24,14 @@ class FGBPSnackBar {
       animationDuration: const Duration(milliseconds: 800),
     );
   }
+}
 
-  static void openOne(String title, String one) {
-    Get.rawSnackbar(
-      onTap: (_) {
-        Get.back();
-      },
-      titleText: Text(title, style: AppTextTheme.bold20),
-      messageText: Text(one, style: AppTextTheme.semiboldMain14),
-      isDismissible: true,
-      backgroundColor: AppColorTheme.white,
-      mainButton: FGBPTextButton(
-        text: "확인",
-        radius: 10,
-        onTap: () {
-          Get.back();
-        },
-      ),
-    );
-  }
-
-  static void openMany(String title, List<String> many) {
-    Get.rawSnackbar(
-      onTap: (_) {
-        Get.back();
-      },
-      titleText: Text(title, style: AppTextTheme.bold20),
-      messageText: Text.rich(
-        TextSpan(children: [
-          TextSpan(text: many[0], style: AppTextTheme.semiboldMain14),
-          TextSpan(text: many[1], style: AppTextTheme.extraBold22),
-          TextSpan(text: many[2], style: AppTextTheme.semiboldMain14),
-        ]),
-      ),
-      isDismissible: true,
-      backgroundColor: AppColorTheme.white,
-      mainButton: FGBPTextButton(
-        text: "확인",
-        radius: 10,
-        onTap: () {
-          Get.back();
-        },
-      ),
+class FGBPErrorSnackBar {
+  void open(String title, {String? message}) {
+    FGBPSnackBar.open(
+      title,
+      backgroundColor: FGBPColors.grey,
+      textColor: Colors.white,
     );
   }
 }
