cf2a8f0193f3d5788c93749c637bd949db18d53c
dhalsdyd <minddong4992@gmail.com>
2023-11-16 21:01:35 +0900

    Pub update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,8 @@ import firebase_core
 import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
